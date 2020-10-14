7892e5c7acd0da9aa62c28fabe6e0e9e41d8d155
Andres Reyna <andresreyna15@gmail.com>
2020-10-14 15:12:40 -0500

    Right Panel fixed, pending content.w

--- static/js/containers.js
+++ static/js/containers.js
@@ -1,34 +1,7 @@
 console.log('Containers JS');
-// TODO: use this techniche, changing dynamically classes
-// and set the transition works the same as with ":hover"
-// JS EXAMPLE
-// document.getElementById('btn').onclick = (e) => {  if(document.getElementById('somebs').classList.contains('someshit-atleft')) {
-//   document.getElementById('somebs').classList.remove("someshit-atleft");
-//  } else {
-//    document.getElementById('somebs').classList.add("someshit-atleft");
-//  }
-// }
+// TODO: ADD builder.html
 //
-// CSS EXAMPLE
-// .someshit {
-//   width: 100px;
-//   height: 100px;
-//   background: blue;
-//   
-//   transition: margin-left 2s;
-// }
-// 
-// .someshit-atleft {
-//   margin-left: 50px;
-// }
-// // HTML EXAPMLE
-// <button id="btn">Move that shit</button>
-// <div id="somebs" class="someshit">
-// <div>
-// TODO: LOAD CONTAINERS AND PUT THEM ON THE containers.html
-// TODO: ADD builder.html 
-// TODO: ADD FUNCTIONALITY TO containers.html
-
+// TODO: ADD LIST VIEW OPTION TO CONTAINER PANEL
 function buildContainerHtmlTemplate(containerinfo) {
 
     let cardHeaderClasses = "";
@@ -41,7 +14,7 @@ function buildContainerHtmlTemplate(containerinfo) {
     if(containerinfo.status == "running") {
         cardActionButton = `
         <button id="pause-${containerinfo.id}" class="btn btn-info">Pause</button>
-        <button id="restart-${containerinfo.id}" class="btn btn-warning" onclick="showLaunchDetails(event, '${containerinfo.id}')">Restart</button>
+        <button id="restart-${containerinfo.id}" class="btn btn-warning" onclick="showLaunchDetails('${containerinfo.id}')">Restart</button>
         <button id="stop-${containerinfo.id}" class="btn btn-danger">Stop</button>`;
     }
     else if(containerinfo.status == "paused") { 
@@ -69,26 +42,6 @@ ${cardActionButton}</div>
 </div></div>`
     return containerHtmlTemplate;
 }
-// TODO: use jinja, like here:
-//    {% if siloStatus == 200: %}
-//    <div class="card-columns">
-//        {% for image in images.repositories %} 
-//            <div class="card bg-light mb-3" style="max-width: 18rem;">
-//                <div class="card-header">UC-{{ image }}</div>
-//                <div class="card-body">
-//                    <h5 class="card-title">{{ image }}</h5>
-//                    <!--<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
-//                </div>
-//            </div>
-//        {% endfor %}
-//    </div>
-//    {% else %}
-//        <h4 class="text-center text-primary">Error al obtener las imágenes del silo!</h4>
-//        {% if images: %}
-//            <p>{{ images.error }}</p>
-//        {% endif %} 
-//    {% endif %}
-
 // suppouse the containers load
 loadContainers();
 function loadContainers() {
@@ -110,30 +63,16 @@ function loadContainers() {
     });
 }
 
-addContainer({"name": "Python Server", "Timeactive": "24H"});
-
-function addContainer(containerinfo) {
-    console.log('container info: ', containerinfo);
-}
-
 // function to show launch details panel.
-function showLaunchDetails(event, containerId) {
-    event.preventDefault();
+function showLaunchDetails(containerId) {
     console.log('container details to show: ', containerId);
-    document.getElementById('rightPanel').style.right = "0px";
+    document.getElementById('rightPanel').style.width = "500px";
+    // document.getElementById('rightPanel').style.display = "flex";
 }
 
 function hideLaunchDetailsPanel() {
-    document.getElementById('rightPanel').style.right = "-600px";
-}
-
-document.getElementById('rightPanel').onclick = (e) => {
-    e.stopPropagation();
-    return false;
+    document.getElementById('rightPanel').style.width = "0";
+    // document.getElementById('rightPanel').style.display = "none";
 }
 
 document.getElementById('btnLaunch').onclick = hideLaunchDetailsPanel;
-document.onclick = (e) => {
-    e.stopPropagation();
-    // hideLaunchDetailsPanel();
-};
