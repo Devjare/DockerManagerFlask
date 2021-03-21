5371e5afa7b3c90fad0881cf469271f7ec748e38
Andres Reyna <andresreyna15@gmail.com>
2021-03-21 17:47:06 -0600

    Minor changes

--- manager/app/constants.py
+++ manager/app/constants.py
@@ -8,4 +8,4 @@ USERID = 'userid'
 # disys0.tamps.cinvestav.mx:2375 <-- IP Servidor
 #LOCALIP = "unix://var/run/docker.sock"
 LOCALIP = os.environ["DOCKER_HOST_IP"]
-PRIVATE_REGISTRY = os.environ["PRIVATE_REGISTRY"] 
+PRIVATE_REGISTRY = "http://" + os.environ["PRIVATE_REGISTRY"] 
