97bdf9a0cf7eace35fa285612fdcc7d199af48d0
Andres Reyna <andresreyna15@gmail.com>
2021-03-21 19:03:59 -0600

    Prod Version docker-compose, removed volumes

--- manager/app/__init__.py
+++ manager/app/__init__.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from flask import Flask, session
+from flask import Flask, session, render_template
 from flask_cors import CORS
 import docker
 from constants import LOCALIP
@@ -21,6 +21,12 @@ def create_app(config_filename):
     app.register_blueprint(images.images_bp, url_prefix="/images")
     app.register_blueprint(containers.containers_bp, url_prefix="/containers")
     app.register_blueprint(login.login_bp)
+
+    # set the main route
+    @app.route('/home')
+    def main():
+        return render_template('index.html')
+
 
     db.init_app(app)
 
--- manager/app/constants.py
+++ manager/app/constants.py
@@ -4,5 +4,9 @@ USERCONTAINERS = 'usercontainers'
 USERNAME = 'username'
 USERID = 'userid'
 
-LOCALIP = os.environ["DOCKER_HOST_IP"]
-PRIVATE_REGISTRY = os.environ["PRIVATE_REGISTRY"] 
+if(os.environ["DOCKER_HOST_IP"]):
+    LOCALIP = os.environ["DOCKER_HOST_IP"]
+else:
+    LOCALIP = "unix://"
+if(os.environ["PRIVATE_REGISTRY"]):
+     PRIVATE_REGISTRY = ("http://" + os.environ["PRIVATE_REGISTRY"])
